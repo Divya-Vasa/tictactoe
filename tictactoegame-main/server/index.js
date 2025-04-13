274103087793c400f0db79db0760bda06d93a129
Divya-Vasa <divyajyothivasa@gmail.com>
2025-04-13 08:56:21 +0530

    Add files via upload

---
+++ tictactoegame-main/server/index.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const app=express();
+const http=require("http");
+const cors=require("cors");
+const {Server} =require("socket.io");
+
+app.use(cors());
+
+const server=http.createServer(app);
+
+const io=new Server(server,{
+    cors:{
+        methods:["GET" , "POST"],
+    },
+});
+
+io.on("connection",(socket)=>{
+    console.log(`USER CONNECTED WITH ID ${socket.id}`);
+
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+    });
+
+    socket.on("send_move",(data)=>{
+        socket.to(data.code).emit("receive_move",data);
+    });
+
+    socket.on("disconnect",()=>{
+        console.log(`USER DISCONNECTED ${socket.id}`);
+    });
+});
+
+server.listen(process.env.PORT,()=>
+{
+    console.log("SERVER CREATED");
+});
